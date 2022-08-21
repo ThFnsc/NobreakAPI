ea3b5683572c7a4cdc3e85629bd34954385a1c90
Thiago Fonseca <thfnsc@outlook.com>
2022-08-21 15:28:37 -0300

    Home controller

---
+++ src/ThFnsc.Nobreak/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThFnsc.Nobreak.Controllers;
+
+[ApiController]
+[Route("/")]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Redirect() =>
+        RedirectPermanent("/Swagger");
+}
